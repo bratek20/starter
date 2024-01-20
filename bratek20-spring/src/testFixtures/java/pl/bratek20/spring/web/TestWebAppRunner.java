2e5ee024f50d7f658ba45ba976a9c9e069c442f8
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-01-20 14:22:39 +0100

    random port for test web app

--- bratek20-spring/src/testFixtures/java/pl/bratek20/spring/web/TestWebAppRunner.java
+++ bratek20-spring/src/testFixtures/java/pl/bratek20/spring/web/TestWebAppRunner.java
@@ -5,22 +5,29 @@ import pl.bratek20.spring.context.SpringContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class TestWebAppRunner {
-    private static int lastPort = 8081;
-
     private final List<Class<?>> configurations;
     @Getter
     private final int port;
 
     public TestWebAppRunner(Class<?>... configurations) {
         this.configurations = new ArrayList<>(List.of(configurations));
-        port = lastPort++;
+        port = generateRandomPort();
     }
 
     public SpringContext run() {
         var app = new WebApp(configurations, new String[]{});
         return app.run(port);
+    }
+
+    public static int generateRandomPort() {
+        int minPort = 8081;
+        int maxPort = 65535;
+
+        Random random = new Random();
+        return random.nextInt(maxPort - minPort + 1) + minPort;
     }
 }
 
