fc6b26ee50100b5b7167fedc1c59b98f46f863a4
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-01-27 12:54:19 +0100

    simplification

--- bratek20-spring/src/main/java/pl/bratek20/spring/config/BaseConfig.java
+++ bratek20-spring/src/main/java/pl/bratek20/spring/config/BaseConfig.java
@@ -3,7 +3,6 @@ package pl.bratek20.spring.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
