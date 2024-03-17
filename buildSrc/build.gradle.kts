b1265fa1fd8350b36fb5e3b89ce8454ebb009caa
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-03-17 13:32:17 +0100

    build src using plugin versions from catalog

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,11 +9,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.freefair.gradle:lombok-plugin:8.3")
-
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.3")
-    implementation("io.spring.gradle:dependency-management-plugin:1.1.3")
+    implementation(libs.lombok.plugin)
+    implementation(libs.spring.boot.plugin)
+    implementation(libs.spring.dependency.management.plugin)
 }
+
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
