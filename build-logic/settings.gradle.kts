ef932a746f2dcbc3287064b684d9ad7a2264ef73
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-03-23 10:03:45 +0100

    taking versions from github

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -11,6 +11,16 @@ dependencyResolutionManagement {
 
     repositories {
         mavenLocal()
+        if (System.getenv("GITHUB_ACTOR") != null) {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/bratek20/starter")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
     }
 }
 
