60a9051c2860aaae126e377aa790e8d68edb02ee
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-03-30 19:08:45 +0100

    tweak

--- bratek20-architecture/src/main/java/pl/bratek20/architecture/exceptions/ApiException.java
+++ bratek20-architecture/src/main/java/pl/bratek20/architecture/exceptions/ApiException.java
@@ -1,6 +1,9 @@
 package pl.bratek20.architecture.exceptions;
 
 public class ApiException extends RuntimeException {
+    public ApiException() {
+        super();
+    }
     public ApiException(String message) {
         super(message);
     }
