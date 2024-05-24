3706d983b4284126adee2271ff8dd8d2860cecd3
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-05-24 05:27:46 +0200

    code refactored by chat

--- bratek20-architecture/src/main/java/pl/bratek20/architecture/properties/sources/yaml/YamlPropertiesSource.kt
+++ bratek20-architecture/src/main/java/pl/bratek20/architecture/properties/sources/yaml/YamlPropertiesSource.kt
@@ -1,15 +1,17 @@
 package pl.bratek20.architecture.properties.sources.yaml
 
 import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.type.TypeFactory
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import lombok.SneakyThrows
 import pl.bratek20.architecture.context.api.ContextBuilder
 import pl.bratek20.architecture.context.api.ContextModule
-import pl.bratek20.architecture.properties.api.*
+import pl.bratek20.architecture.properties.api.ListPropertyKey
+import pl.bratek20.architecture.properties.api.ObjectPropertyKey
+import pl.bratek20.architecture.properties.api.PropertiesSource
+import pl.bratek20.architecture.properties.api.PropertiesSourceName
 import java.io.File
-import java.io.IOException
 import java.lang.reflect.Type
 import kotlin.reflect.KClass
 
@@ -17,96 +19,77 @@ class YamlPropertiesSource(
     private val propertiesPath: String
 ) : PropertiesSource {
 
+    private val mapper = ObjectMapper(YAMLFactory())
+
     override fun getName(): PropertiesSourceName {
         return PropertiesSourceName("yaml")
     }
 
     override fun hasKey(keyName: String): Boolean {
-        return privateHasKey(keyName)
+        return checkYamlPathExists(keyName)
     }
 
     override fun <T : Any> isObjectOfType(keyName: String, type: KClass<T>): Boolean {
-        return hasOfType(keyName, objectTypeRef(type.java))
+        return checkType(keyName, objectTypeRef(type.java))
     }
 
     override fun <T : Any> isListWithWrappedType(keyName: String, type: KClass<T>): Boolean {
-        return hasOfType(keyName, listTypeRef(type.java))
+        return checkType(keyName, listTypeRef(type.java))
     }
 
     override fun <T : Any> getList(key: ListPropertyKey<T>): List<T> {
-        return parseYamlSection(propertiesPath, key.name, listTypeRef(key.elementType.java))
+        return parseYamlSection(key.name, listTypeRef(key.elementType.java))
     }
 
     override fun <T : Any> getObject(key: ObjectPropertyKey<T>): T {
-        return parseYamlSection(propertiesPath, key.name, objectTypeRef(key.type.java))
+        return parseYamlSection(key.name, objectTypeRef(key.type.java))
     }
 
-    private fun privateHasKey(keyName: String): Boolean {
-        val name = keyName
-        val mapper = ObjectMapper(YAMLFactory())
-        // Load the YAML file
-        val file = File(javaClass.classLoader.getResource(propertiesPath).file)
-        val rootNode = mapper.readTree(file)
+    private fun checkYamlPathExists(keyName: String): Boolean {
+        return try {
+            val file = getFile(propertiesPath)
+            val rootNode = mapper.readTree(file)
+            navigateToYamlNode(rootNode, keyName) != null
+        } catch (e: Exception) {
+            false
+        }
+    }
 
-        // Navigate to the specified YAML path
-        val pathElements = name.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    private fun <T> checkType(keyName: String, typeReference: TypeReference<T>): Boolean {
+        return try {
+            val file = getFile(propertiesPath)
+            val rootNode = mapper.readTree(file)
+            val targetNode = navigateToYamlNode(rootNode, keyName)
+            targetNode != null && mapper.treeToValue(targetNode, typeReference) != null
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    private fun <T> parseYamlSection(yamlPath: String, typeReference: TypeReference<T>): T {
+        val file = getFile(propertiesPath)
+        val rootNode = mapper.readTree(file)
+        val targetNode = navigateToYamlNode(rootNode, yamlPath)
+        return mapper.convertValue(targetNode, typeReference)
+    }
+
+    private fun getFile(path: String): File {
+        return File(javaClass.classLoader.getResource(path).file)
+    }
+
+    private fun navigateToYamlNode(rootNode: JsonNode, path: String): JsonNode? {
+        val pathElements = path.split(".")
         var targetNode = rootNode
         for (element in pathElements) {
-            targetNode = targetNode!![element]
+            targetNode = targetNode[element]
             if (targetNode == null) {
-                return false // Path does not exist
+                return null
             }
         }
-
-        return true
-    }
-
-    fun <T> hasOfType(keyName: String, typeReference: TypeReference<T>): Boolean {
-        val name = keyName
-        val mapper = ObjectMapper(YAMLFactory())
-        try {
-            // Load the YAML file
-            val file = File(javaClass.classLoader.getResource(propertiesPath).file)
-            val rootNode = mapper.readTree(file)
-
-            // Navigate to the specified YAML path
-            val pathElements = name.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            var targetNode = rootNode
-            for (element in pathElements) {
-                targetNode = targetNode!![element]
-                if (targetNode == null) {
-                    return false // Path does not exist
-                }
-            }
-
-            // Attempt to parse the node into the specified type
-            val parsedObject = mapper.treeToValue(targetNode, typeReference)
-            return parsedObject != null // If parsing succeeds, the path exists and is of the specified type
-        } catch (e: IOException) {
-            return false // Parsing failed or path does not exist
-        } catch (e: IllegalArgumentException) {
-            return false
-        }
+        return targetNode
     }
 
     companion object {
-        @SneakyThrows
-        fun <T> parseYamlSection(resourcePath: String?, yamlPath: String, typeReference: TypeReference<T>): T {
-            val mapper = ObjectMapper(YAMLFactory())
-            val file = File(YamlPropertiesSource::class.java.classLoader.getResource(resourcePath).file)
-            val rootNode = mapper.readTree(file)
-
-            // Navigate to the specified YAML path
-            val pathElements = yamlPath.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            var targetNode = rootNode
-            for (element in pathElements) {
-                targetNode = targetNode.path(element)
-            }
-
-            // Deserialize the target node into the specified type
-            return mapper.convertValue(targetNode, typeReference)
-        }
-
         fun <T> objectTypeRef(clazz: Class<T>): TypeReference<T> {
             return object : TypeReference<T>() {
                 override fun getType(): Type {
