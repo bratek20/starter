b744b99da3c0fe9a094eeceadad2d9bffb06ccf6
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-06-18 20:50:09 +0200

    source loggers, TODO: more tests to prove impl not finished

--- bratek20-logs/logs-core/src/main/kotlin/com/github/bratek20/logs/slf4j/Slf4jLogger.kt
+++ bratek20-logs/logs-core/src/main/kotlin/com/github/bratek20/logs/slf4j/Slf4jLogger.kt
@@ -5,8 +5,15 @@ import org.slf4j.LoggerFactory
 
 class Slf4jLogger : Logger {
     private val defaultLogger = LoggerFactory.getLogger(Slf4jLogger::class.java)
+    private val sourceLoggers = mutableMapOf<Any, org.slf4j.Logger>()
 
     override fun info(message: String, source: Any?) {
+        if (source != null) {
+            sourceLoggers.getOrPut(source) {
+                LoggerFactory.getLogger(source::class.java)
+            }.info(message)
+            return
+        }
         defaultLogger.info(message)
     }
 
--- bratek20-logs/logs-core/src/test/kotlin/com/github/bratek20/logs/Slf4jLoggerTest.kt
+++ bratek20-logs/logs-core/src/test/kotlin/com/github/bratek20/logs/Slf4jLoggerTest.kt
@@ -26,4 +26,26 @@ class Slf4jLoggerTest {
         assertThat(logCaptor.warnLogs).containsExactly("test warn")
         assertThat(logCaptor.errorLogs).containsExactly("test error")
     }
+
+    class Source1
+    class Source2
+
+    @Test
+    fun `should log message for correct source`() {
+        val logger = someContextBuilder()
+            .withModule(Slf4jLogsImpl())
+            .get(Logger::class.java)
+
+        val defaultLogCaptor = LogCaptor.forClass(Slf4jLogger::class.java)
+        val source1LogCaptor = LogCaptor.forClass(Source1::class.java)
+        val source2LogCaptor = LogCaptor.forClass(Source2::class.java)
+
+        logger.info("default")
+        logger.info("source1", Source1())
+        logger.info("source2", Source2())
+
+        assertThat(defaultLogCaptor.infoLogs).containsExactly("default")
+        assertThat(source1LogCaptor.infoLogs).containsExactly("source1")
+        assertThat(source2LogCaptor.infoLogs).containsExactly("source2")
+    }
 }
