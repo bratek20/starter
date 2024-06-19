60c2612f72b6065b50a5dfb17b5179008a4c322a
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-06-19 08:16:12 +0200

    url tweak

--- build-logic/conventions/build.gradle.kts
+++ build-logic/conventions/build.gradle.kts
@@ -88,7 +88,7 @@ publishing {
         if (project.hasProperty("centralUsername")) {
             maven {
                 name = "central"
-                url = uri("https://artifactory.devs.tensquaregames.com/artifactory/libs-release-local")
+                url = uri("https://artifactory.devs.tensquaregames.com/artifactory/plugins-release-local")
                 credentials {
                     username = project.findProperty("centralUsername") as String
                     password = project.findProperty("centralPassword") as String
