7081ea73ba422a20624141f0c1a2432458541e6b
Bartosz Rudzki (Legion) <bartosz.rudzki@tensquaregames.com>
2024-06-29 14:01:17 +0200

    other test also not passing

--- bratek20-spring/src/test/java/com/github/bratek20/spring/webapp/SpringBootWebBugTest.kt
+++ bratek20-spring/src/test/java/com/github/bratek20/spring/webapp/SpringBootWebBugTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,8 +21,18 @@ class TestHealthController {
 }
 
 @Configuration
+open class TestHealthControllerConfig {
+    @Bean
+    open fun testHealthController(): TestHealthController {
+        return TestHealthController()
+    }
+}
+
+@Configuration
 @EnableAutoConfiguration
 open class TestWebAppConfig
+
+
 
 class SpringBootWebBugTest {
     @Test
@@ -43,6 +54,19 @@ class SpringBootWebBugTest {
             .run("--server.port=8078")
 
         assertHealth(8078)
+    }
+
+    @Test
+    fun healthControllerAsParent_config() {
+        val context = AnnotationConfigApplicationContext()
+        context.register(TestHealthControllerConfig::class.java)
+        context.refresh()
+
+        SpringApplicationBuilder(TestWebAppConfig::class.java)
+            .parent(context)
+            .run("--server.port=8079")
+
+        assertHealth(8079)
     }
 
     private fun assertHealth(port: Int) {
