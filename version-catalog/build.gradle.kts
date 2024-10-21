c54b55f3a5ebfe334134b59cfec6536dac9c589a
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-10-21 19:52:11 +0200

    publish

--- version-catalog/build.gradle.kts
+++ version-catalog/build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 }
 
 //changed by script
-val catalogVersion = "1.0.71"
+val catalogVersion = "1.0.72"
 
 //changed manually
-val b20ArchVersion = "1.0.52"
+val b20ArchVersion = "1.0.53"
 val b20InfraVersion = "1.0.50"
 val b20SpringVersion = "1.0.50"
 val b20UtilsVersion = "1.0.50"
