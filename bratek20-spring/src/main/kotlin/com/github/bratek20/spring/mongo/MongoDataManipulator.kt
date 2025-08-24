4dd909482b0722a9500e7f0f386f37346ad102fd
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2025-08-24 16:34:49 +0200

    tweak

--- bratek20-spring/src/main/kotlin/com/github/bratek20/spring/mongo/MongoDataManipulator.kt
+++ bratek20-spring/src/main/kotlin/com/github/bratek20/spring/mongo/MongoDataManipulator.kt
@@ -17,14 +17,16 @@ class MongoDataManipulator(
     private val logger: Logger // TODO-REF move times logging to DataStorage + move logs to architecture
 ) : DataManipulator {
 
-    private val collectionName = "kv_store"
+    companion object {
+        private const val COLLECTION_NAME = "kv_store"
+    }
 
     override fun findValue(keyName: String): SerializedValue? {
         val start = System.currentTimeMillis()
         require(keyName.isNotBlank()) { "keyName must not be blank" }
 
         // {_id: keyName, v: <Document>}
-        val doc: Document? = mongoTemplate.findById(keyName, Document::class.java, collectionName)
+        val doc: Document? = mongoTemplate.findById(keyName, Document::class.java, COLLECTION_NAME)
         val payload: Document = doc?.get("v", Document::class.java) ?: return null
 
         // Convert back to JSON string for your SerializedValue
@@ -39,7 +41,7 @@ class MongoDataManipulator(
 
         // Null value means delete the key entirely
         if (value == null) {
-            mongoTemplate.remove(Query(Criteria.where("_id").`is`(keyName)), collectionName)
+            mongoTemplate.remove(Query(Criteria.where("_id").`is`(keyName)), COLLECTION_NAME)
             return
         }
 
@@ -60,7 +62,7 @@ class MongoDataManipulator(
         // Upsert {_id: keyName, v: parsed}
         val q = Query(Criteria.where("_id").`is`(keyName))
         val u = Update().set("v", parsed)
-        mongoTemplate.upsert(q, u, collectionName)
+        mongoTemplate.upsert(q, u, COLLECTION_NAME)
         val end = System.currentTimeMillis()
         logger.debug("MongoDataManipulator.setValue('$keyName') took ${end - start}ms", this)
     }
